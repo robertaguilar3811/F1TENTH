f9a841df099facd9a2ee87b70d690608994f7e2b
robertaguilar3811 <robertaguilar3811@gmail.com>
2025-08-10 09:06:36 -0700

    added launch file

--- src/car_control/setup.py
+++ src/car_control/setup.py
@@ -7,10 +7,10 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+    ('share/' + package_name + '/launch', ['launch/usb_cam_launch.py']),  # Add this line
+],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='rj',
