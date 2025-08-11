12aacd1b586be65036c4dbf451a33b22dfb2f61d
robertaguilar3811 <robertaguilar3811@gmail.com>
2025-08-10 17:13:55 -0700

    created bringup folder

---
+++ src/bringup/launch/bringup_launch.py
@@ -0,0 +1,13 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        # Image viewer node
+        Node(
+            package='car_control',
+            executable='image_viewer',
+            name='image_viewer',
+            output='screen',
+        ),
+    ])
